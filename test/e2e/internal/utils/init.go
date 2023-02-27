496be705a1028178025bb371e8d8ca5a8f6374ad
Billy Zha <jinzha1@microsoft.com>
2023-02-27 07:45:46 +0000

    try main and internal
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/internal/utils/init.go
+++ test/e2e/internal/utils/init.go
@@ -102,7 +102,7 @@ func init() {
 			// fallback to native build to facilitate local debugging
 			buildArgs := []string{}
 			if CovDumpPath != "" {
-				buildArgs = append(buildArgs, "-cover", "-coverpkg", "all")
+				buildArgs = append(buildArgs, "-cover", "-coverpkg", "main,oras.land/oras/cmd/oras/internal")
 			}
 			ORASPath, err = gexec.Build("oras.land/oras/cmd/oras", buildArgs...)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
