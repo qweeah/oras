5bf39ef5e2e5d03b764e381ac3aea728d8e21df0
Billy Zha <jinzha1@microsoft.com>
2023-02-27 07:54:47 +0000

    see if report can be merged
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/scripts/e2e.sh
+++ test/e2e/scripts/e2e.sh
@@ -63,7 +63,7 @@ ginkgo -r -p --succinct suite || fail=true
 if ! [ -z ${COVERAGE_DUMP_ROOT} ]; then
   echo " === generating code cov report === "
   cov_path="${repo_root}/test/e2e/coverage.txt"
-  go tool covdata textfmt -i="${repo_root}/test/e2e/${COVERAGE_DUMP_ROOT}" -o $cov_path || true 
+  (go tool covdata textfmt -i="${repo_root}/test/e2e/${COVERAGE_DUMP_ROOT}" -o $cov_path && cat $cov_path | sed 's/set/atomic/' > $cov_path) || true
 fi
 
 if [ "${fail}" = 'true' ]; then
