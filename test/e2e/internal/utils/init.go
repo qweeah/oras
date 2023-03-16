a8f7bd0a58f22923f59b4fa554978e142eb60711
Billy Zha <jinzha1@microsoft.com>
2023-03-16 03:21:38 +0000

    pt test: oras.land/oras/cmd/oras
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- test/e2e/internal/utils/init.go
+++ test/e2e/internal/utils/init.go
@@ -102,7 +102,7 @@ func init() {
 			// fallback to native build to facilitate local debugging
 			buildArgs := []string{}
 			if CovDumpPath != "" {
-				buildArgs = append(buildArgs, "-coverpkg", "oras.land/oras")
+				buildArgs = append(buildArgs, "-coverpkg", "oras.land/oras/cmd/oras")
 			}
 			ORASPath, err = gexec.Build("oras.land/oras/cmd/oras", buildArgs...)
 			gomega.Expect(err).NotTo(gomega.HaveOccurred())
