681d2211e3f6ec0d96c510d848d7851c5bf1e4ed
Billy Zha <jinzha1@microsoft.com>
2023-03-20 06:58:03 +0000

    add unit test to cover more
    Signed-off-by: Billy Zha <jinzha1@microsoft.com>

--- internal/crypto/certificate_test.go
+++ internal/crypto/certificate_test.go
@@ -17,11 +17,13 @@ package crypto
 
 import (
 	"context"
+	"crypto/x509"
 	"encoding/pem"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 )
 
@@ -55,5 +57,36 @@ func TestLoadCertPool(t *testing.T) {
 	_, err = client.Do(req)
 	if err != nil {
 		t.Fatalf("failed to trust the self signed pem: %v", err)
+	}
+}
+
+func TestLoadCertPool_err(t *testing.T) {
+	pemPath := filepath.Join(t.TempDir(), "invalid.pem")
+	if err := os.WriteFile(pemPath, []byte{}, 0644); err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	type args struct {
+		path string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *x509.CertPool
+		wantErr bool
+	}{
+		{"should fail if the path is doesn't exist", args{"/???"}, nil, true},
+		{"should fail if the file is not a pem", args{pemPath}, nil, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := LoadCertPool(tt.args.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("LoadCertPool() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("LoadCertPool() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
